9f424d250944e06835868a8da0e1e26e2626f104
Yashraj Zope <105965863+Technoband@users.noreply.github.com>
2024-04-16 10:46:40 +0530

    Update MNT_MDT.py

--- MNT_MDT.py
+++ MNT_MDT.py
@@ -1,49 +1,50 @@
-def generate_mnt_mdt(alp):
-    mnt = {}  # Macro Name Table
-    mdt = {}  # Macro Definition Table
-    macro_name = None
-    parameters = []
-    inside_macro = False
+def display_MNT_and_MDT(ALP):
+    MNT = {}  # Macro Name Table
+    MDT = {}  # Macro Definition Table
+    current_macro_name = None  # Track current macro being defined
 
-    for line in alp.split('\n'):
-        if line.startswith('MACRO'):
+    for line_index, line in enumerate(ALP):
+        if line.startswith("MACRO"):
             parts = line.split()
             macro_name = parts[1]
             parameters = parts[2:]
-            mnt[macro_name] = len(parameters)
-            mdt[macro_name] = []
-            inside_macro = True
-        elif line.startswith('MEND'):
-            inside_macro = False
-        elif inside_macro:
-            mdt[macro_name].append(line)
+            current_macro_name = macro_name
+            MNT[macro_name] = line_index  # Index of MDT entry for this macro
+            MDT[macro_name] = {'parameters': parameters, 'definition': []}
+        elif line == "MEND":
+            current_macro_name = None
+        else:
+            if current_macro_name is not None:
+                MDT[current_macro_name]['definition'].append(line)
 
-    return mnt, mdt
-
-def display_mnt(mnt):
+    # Print MNT
     print("Macro Name Table (MNT):")
-    print("Macro Name\t#Parameters")
-    for macro, params in mnt.items():
-        print(f"{macro}\t\t{params}")
+    for macro_name, index in MNT.items():
+        print(f"{macro_name}: {index}")
 
-def display_mdt(mdt):
+    # Print MDT
     print("\nMacro Definition Table (MDT):")
-    print("Macro Name\tDefinition")
-    for macro, definition in mdt.items():
-        print(f"{macro}\t\t{' '.join(definition)}")
+    for macro_name, data in MDT.items():
+        print(f"{macro_name}:")
+        print("Parameters:", data['parameters'])
+        print("Definition:")
+        for line in data['definition']:
+            print(line)
 
-if __name__ == "__main__":
-    alp = """
-    MACRO ADD A, B
-        MOV A, B
-    MEND
+# Example usage:
+ALP = [
+    "MACRO ADD A, B",
+    "LDA A",
+    "ADD B",
+    "STA RESULT",
+    "MEND",
+    "MACRO SUB A, B",
+    "LDA A",
+    "SUB B",
+    "STA RESULT",
+    "MEND",
+    "ADD 5, 10",
+    "SUB 20, 7"
+]
 
-    MACRO SUB A, B
-        SUB A, B
-    MEND
-    """
-
-    mnt, mdt = generate_mnt_mdt(alp)
-    display_mnt(mnt)
-    display_mdt(mdt)
-
+display_MNT_and_MDT(ALP)
